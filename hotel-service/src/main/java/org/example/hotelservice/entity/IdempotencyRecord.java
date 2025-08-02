340a9ee520a01f61ead2997cf1a9379e812203bc
rajkundalia <rajakundalia@gmail.com>
2025-08-02 21:34:28 +0530

    Replacing Data annotation as per suggestion in hotel service

--- hotel-service/src/main/java/org/example/hotelservice/entity/IdempotencyRecord.java
+++ hotel-service/src/main/java/org/example/hotelservice/entity/IdempotencyRecord.java
@@ -1,13 +1,27 @@
 package org.example.hotelservice.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "idempotency_records")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class IdempotencyRecord {
     @Id
     private String idempotencyKey;
@@ -20,5 +34,18 @@ public class IdempotencyRecord {
     @PrePersist
     public void prePersist() {
         processedAt = LocalDateTime.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        IdempotencyRecord that = (IdempotencyRecord) o;
+        return idempotencyKey != null && Objects.equals(idempotencyKey, that.idempotencyKey);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
--- hotel-service/src/main/java/org/example/hotelservice/entity/Reservation.java
+++ hotel-service/src/main/java/org/example/hotelservice/entity/Reservation.java
@@ -1,16 +1,33 @@
 package org.example.hotelservice.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.example.hotelservice.enumeration.ReservationStatus;
+import org.hibernate.Hibernate;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "reservations")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class Reservation {
     @Id
     private String reservationId;
@@ -40,5 +57,18 @@ public class Reservation {
     @PreUpdate
     public void preUpdate() {
         updatedAt = LocalDateTime.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Reservation that = (Reservation) o;
+        return reservationId != null && Objects.equals(reservationId, that.reservationId);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
